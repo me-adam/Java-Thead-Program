116b41a780d5ce3adc42cd2b8ea7bbd0f62daf36
Mustafa Adam <44250738+me-adam@users.noreply.github.com>
2020-05-26 07:44:14 +0200

    Add files via upload

---
+++ JavaThread.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class jaa
+{
+public static void main(String s[]){
+Scanner sl=new Scanner(System.in);
+m l=new m();
+for(int i=0;i<10;i++){
+int x=sl.nextInt();
+
+}
+
+
+
+
+
+}
+
+static class m extends Thread
+{
+public m(){
+	run();
+}
+public void run(){
+		try{
+
+for(int i=0;i<90;i++){
+System.out.println("javaa king ");
+Thread.sleep(100);
+}
+}catch(Exception e){System.out.println("expiton");}
+
+
+}
+
+
+}
+}
